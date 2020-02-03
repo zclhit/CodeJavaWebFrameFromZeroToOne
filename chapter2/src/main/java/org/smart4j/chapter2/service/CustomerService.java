0b208f48b24e596e1e16306baefea59514bf020d
changle.zhang <changle.c@thoughtworks.com>
2020-02-03 14:52:32 +0800

    [zclhit] #feat: init project

---
+++ chapter2/src/main/java/org/smart4j/chapter2/service/CustomerService.java
@@ -0,0 +1,44 @@
+package org.smart4j.chapter2.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.smart4j.chapter2.helper.DatabaseHelper;
+import org.smart4j.chapter2.model.Customer;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 提供客户数据服务
+ * @author wukong
+ * @since 2017-06-29.
+ */
+public class CustomerService {
+    private static Logger logger = LoggerFactory.getLogger(CustomerService.class);
+
+    /**
+     * 获取客户列表
+     * @return
+     */
+    public List<Customer> getCustomerList() {
+        String sql = "SELECT * FROM customer";
+        return DatabaseHelper.queryEntityList(Customer.class, sql);
+    }
+
+    public Customer getCustomer(long id) {
+        String sql = "SELECT * FROM customer WHERE id = " + id;
+        return DatabaseHelper.queryEntity(Customer.class, sql);
+    }
+
+    public boolean createCustomer(Map<String, Object> fieldMap) {
+        return DatabaseHelper.insertEntity(Customer.class, fieldMap);
+    }
+
+    public boolean updateCustomer(long id, Map<String, Object> fieldMap) {
+        return DatabaseHelper.updateEntity(Customer.class, id, fieldMap);
+    }
+
+    public boolean deleteCustomer(long id) {
+        return DatabaseHelper.deleteEntity(Customer.class, id);
+    }
+}
