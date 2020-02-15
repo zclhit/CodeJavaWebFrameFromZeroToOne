d632cecf237e757c5f32a01a578b63837c6e3caa
changle.zhang <changle.c@thoughtworks.com>
2020-02-15 11:29:52 +0800

    [zclhit] #feat: init project

--- chapter1/src/main/java/org/smart4j/chapter1/HelloServlet.java
+++ chapter1/src/main/java/org/smart4j/chapter1/HelloServlet.java
@@ -10,22 +10,13 @@ import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-/**
- * @author wukong
- * @since 2017-06-27.
- */
 @WebServlet("/hello")
 public class HelloServlet extends HttpServlet {
-
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         String currentTime = dateFormat.format(new Date());
-        request.setAttribute("currentTime", currentTime);
-        request.getRequestDispatcher("/WEB-INF/jsp/hello.jsp").forward(request, response);
-    }
-
-    @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doPost(req, resp);
+        req.setAttribute("currentTime", currentTime);
+        req.getRequestDispatcher("/WEB-INF/jsp/hello.jsp").forward(req, resp);
     }
 }
--- chapter1/src/main/webapp/WEB-INF/jsp/hello.jsp
+++ chapter1/src/main/webapp/WEB-INF/jsp/hello.jsp
@@ -1,17 +1,12 @@
-<%--
-  Created by IntelliJ IDEA.
-  User: lzming
-  Date: 2017/6/27
-  Time: 下午6:44
-  To change this template use File | Settings | File Templates.
---%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
 <head>
-    <title>Title</title>
+    <title>Hello</title>
 </head>
 <body>
-    <h1>Hello!</h1>
-    <h2>当前时间: ${currentTime}</h2>
+<h1>Hello!</h1>
+
+<h2>当前时间: ${currentTime}</h2>
+
 </body>
 </html>
